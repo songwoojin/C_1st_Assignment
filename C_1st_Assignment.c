63a284ab3615bdfbfcf6b087f335525168e6798f
songwoojin <31681788+songwoojin@users.noreply.github.com>
2017-09-06 14:56:34 +0900

    Add files via upload

---
+++ C_1st_Assignment.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+printf("student ID=2017301039\n");
+printf("name=song woo jin\n");
+printf("phone number=010-9243-4912\n");
+printf("github ID=songwoojin\n");
+return 0;
+}
